24270179ed0531241d72f24f590184b0cea95db1
Anindya Ghosh <rijuanindyaghosh@gmail.com>
2026-09-09 13:44:07 +0530

    Fixed cors link

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "http://teachers-anni.vercel.app",
+        "https://teachers-anni.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type"],
